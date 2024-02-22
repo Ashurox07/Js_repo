87a2e8cdda2f70fb27883c5b8b9bc8603d634e38
Ashis Pradhan <119153430+Ashurox07@users.noreply.github.com>
2024-02-22 15:21:59 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,29 @@
+let score = null;
+
+// "55" => 55
+// above score value if you give "55abc" then it will show the output is NaN
+// null the it's output is  0.
+// boolean value true then it's output is 1 if false then 0.
+
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+let isLoggedIn  = 1;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "Ashis" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
+
